2aba58bdcd999faf643236664f656835020ab6bd
liyxmse <liyxmse@amazon.com>
2023-10-12 16:40:10 +0800

    add lambda authorizer config guide

---
+++ lambda_auth/lambda.py
@@ -0,0 +1,20 @@
+import json
+
+def lambda_handler(event, context):
+    
+    token = event.get('queryStringParameters').get("token", "")
+    # 如有需要可实现更高级别的校验，比如正则匹配、参数+时间 哈西
+    if token == '123456':
+      return {
+        "isAuthorized": True,
+        "context": {
+          "exampleKey": "exampleValue"
+        }
+      }
+    else : 
+      return {
+        "isAuthorized": False,
+        "context": {
+          "exampleKey": "exampleValue"
+        }
+      }
