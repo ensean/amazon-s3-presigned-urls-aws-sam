b230d4d868149074d089db1ad3412aecf78b544b
liyxmse <liyxmse@amazon.com>
2023-10-12 17:02:57 +0800

    update org

--- lambda_auth/lambda.py
+++ lambda_auth/lambda.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     
     token = event.get('queryStringParameters').get("token", "")
-    # 如有需要可实现更高级别的校验，比如正则匹配、参数+时间 哈西
+    # 如有需要可实现更高级别的校验，比如正则匹配、参数+时间 哈希
     if token == '123456':
       return {
         "isAuthorized": True,
